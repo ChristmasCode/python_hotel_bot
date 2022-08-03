7e117618d4784aca34e02621f08f17ab1f44218f
Christmas-PC <alexandr.gothenya@gmail.com>
2022-08-03 23:29:09 +0300

    bot in progress

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 import telebot
 from telebot import types
-# from bot_requests.low_price import low_price
-
+from bot_requests.low_price import get_city, get_properties, answer_low_hotel_list
 
 TOKEN = '5511162987:AAGtehigXviygciyEJHdfBRgr8zVwzJtdh4'
 bot = telebot.TeleBot(TOKEN)
@@ -21,7 +20,7 @@ def get_start_messages(message):
 
 @bot.message_handler(commands=["start"])
 def get_commands_messages(message):
-    keyboard = types.InlineKeyboardMarkup(row_width=2)
+    keyboard = types.InlineKeyboardMarkup(row_width=1)
     key_lowprice = types.InlineKeyboardButton(text="/lowprice", callback_data="lowprice")
     key_highprice = types.InlineKeyboardButton(text="/highprice", callback_data="highprice")
     key_bestdeal = types.InlineKeyboardButton(text="/bestdeal", callback_data="bestdeal")
@@ -33,19 +32,45 @@ def get_commands_messages(message):
 @bot.callback_query_handler(func=lambda call: True)
 def answer(call):
     if call.data == "lowprice":
-        bot.send_message(call.message.chat.id, "lowprice")
-    if call.data == "highprice":
+        mesg = bot.send_message(call.from_user.id, 'Enter the city where you want to search: ')
+        bot.register_next_step_handler(mesg, low_price_city_request)
+    elif call.data == "highprice":
         bot.send_message(call.message.chat.id, "highprice")
-    if call.data == "bestdeal":
+    elif call.data == "bestdeal":
         bot.send_message(call.message.chat.id, "bestdeal")
-    if call.data == "history":
+    elif call.data == "history":
         bot.send_message(call.message.chat.id, "history")
+    # else:
+    #     get_properties(int(call.data))
     bot.answer_callback_query(callback_query_id=call.id)
 
 
 @bot.message_handler(commands=["help"])
 def get_text_messages(message):
     bot.send_message(message.from_user.id, bot_help)
+
+
+@bot.message_handler(commands=["lowprice"])
+def low_price_city_request(message):
+    cities_districts = get_city(message.text)
+    print("BOT LOG: cities_districts ", cities_districts)
+    keyboard = types.InlineKeyboardMarkup(row_width=1)
+    keys = {}
+    for city_name, city_id in cities_districts.items():
+        # button = types.InlineKeyboardButton(text=city_name, callback_data=city_id)
+        # keyboard.add(button)
+        keys[city_id] = types.InlineKeyboardButton(text=str(city_name), callback_data=city_id)
+        keyboard.add(keys[city_id])
+    mesg = bot.send_message(message.chat.id, 'Make your choice:',  reply_markup=keyboard)
+    bot.register_next_step_handler(mesg, low_price_city_answer)
+    # print(mesg.text)
+    # print(keys)in
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def low_price_city_answer(message):
+    mesg = message.text
+    bot.send_message(message.chat.id, mesg)
 
 
 bot.polling(none_stop=True, interval=0)
--- bot_requests/low_price.py
+++ bot_requests/low_price.py
@@ -1,137 +1,143 @@
 import json
 import requests
-from telebot import TeleBot
 from bot_requests.photos_request import want_photos, get_hotel_photo
 
 
-def low_price():
+# low_price:
+#
+#     """
+#     Script lowprice.
+#         Requested:
+#             - City to be searched.
+#
+#             - Number of hotels to withdraw as a result (no more
+#             a predetermined maximum).
+#
+#             - Need to upload and output photos for each hotel («Yes/No»):
+#                 - If the answer is positive, the user also enters the number
+#                 required photos (no more than the
+#                 maxima).
+#     """
 
-    """
-    Function lowprice.
-        Requested:
-            - City to be searched.
+def get_location(city):
+    print('get_location log:', city)
+    url = "https://hotels4.p.rapidapi.com/locations/v2/search"
 
-            - Number of hotels to withdraw as a result (no more
-            a predetermined maximum).
+    querystring = {
+        "query": str(city),
+        "locale": "en_US",
+        "currency": "USD"
+    }
 
-            - Need to upload and output photos for each hotel («Yes/No»):
-                - If the answer is positive, the user also enters the number
-                required photos (no more than the
-                maxima).
-    """
+    headers = {
+        "X-RapidAPI-Key": "308a0ecd4dmsh778f749df86bb27p1e009ajsn881d6b34ec98",
+        "X-RapidAPI-Host": "hotels4.p.rapidapi.com"
+    }
 
-    def get_location():
+    response = requests.request("GET", url, headers=headers, params=querystring, timeout=10)
+    result = json.loads(response.text)
 
-        url = "https://hotels4.p.rapidapi.com/locations/v2/search"
+    # with open('location_answer', 'w+') as file:
+    #     json.dump(result, file, indent=4)
+    print('get_location result log:', result)
+    return result
 
-        querystring = {
-            "query": input('Enter the city where you want to search: '),
-            "locale": "en_US",
-            "currency": "USD"
+
+def get_city(city):
+    print('get_city log:', city)
+    result_location = get_location(city)
+    data_suggestions = result_location.get('suggestions')
+    group_city_data = data_suggestions[0]
+    print('group_city_names log:', group_city_data)
+    data_city_name = [name.get('name') for name in group_city_data.get('entities')]  # returns a list of cities
+    # (city districts)
+    print('data_city_name log', data_city_name)
+    data_destinationId = [destinationId.get('destinationId') for destinationId in group_city_data.get('entities')]  #
+    # returns the ID of cities(city districts)
+    data_city_id = data_destinationId
+    print('data_city_id log:', data_city_id)
+    result = dict(zip(data_city_name, data_city_id))
+    print('get_city result log', result)
+
+    return result
+
+
+def get_properties(city_id):
+    url = "https://hotels4.p.rapidapi.com/properties/list"
+
+    querystring = {
+        "destinationId": city_id,
+        "pageNumber": "1",
+        "pageSize": "3",
+        "checkIn": "2022-08-08",
+        "checkOut": "2022-08-15",
+        "adults1": "1",
+        "sortOrder": "PRICE",
+        "locale": "en_US",
+        "currency": "USD"
+    }
+
+    headers = {
+        "X-RapidAPI-Key": "308a0ecd4dmsh778f749df86bb27p1e009ajsn881d6b34ec98",
+        "X-RapidAPI-Host": "hotels4.p.rapidapi.com"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring, timeout=10)
+    result = json.loads(response.text)
+
+    # with open('location_answer', 'w+') as file:
+    #     json.dump(result, file, indent=4)
+    print(result)
+    return result
+
+
+def get_hotels():
+    result_hotels = get_properties()
+
+    result = (result_hotels['data']['body']['searchResults']['results'])
+    # with open('hotel_answer', 'w+') as file:
+    #     json.dump(result, file, indent=4)
+
+    return result
+
+
+def answer_low_hotel_list():
+    hotel_answer = get_hotels()
+
+    photo_count = want_photos(input("you want to see photos of hotels? ('yes'/'no'): ").lower())
+
+    answer = {}
+    for cur_hotel in hotel_answer:
+        hotel_id = cur_hotel.get("id")
+        address = cur_hotel.get("address")
+
+        current_price = "no price"
+        ratePlan = cur_hotel.get("ratePlan")
+        if not ratePlan:
+            answer.get("Price", "no price")
+        else:
+            price = ratePlan.get("price")
+            current_price = price.get("current")
+
+        guest_rating = "no rating"
+        guestReviews = cur_hotel.get("guestReviews")
+        if not guestReviews:
+            answer.get("Guest Rating", "no rating")
+        else:
+            guest_rating = guestReviews.get("rating")
+
+        answer = {
+            "Hotel name": cur_hotel.get("name"),
+            "Address": address.get("streetAddress", "no address"),
+            "Price": current_price,
+            "Guest rating": guest_rating
         }
+        for answer_key, answer_value in answer.items():
+            print(answer_key + ":", answer_value)
 
-        headers = {
-            "X-RapidAPI-Key": "308a0ecd4dmsh778f749df86bb27p1e009ajsn881d6b34ec98",
-            "X-RapidAPI-Host": "hotels4.p.rapidapi.com"
-        }
+        if photo_count > 0:
+            get_hotel_photo(hotel_id, photo_count)
 
-        response = requests.request("GET", url, headers=headers, params=querystring)
-        result = json.loads(response.text)
+    # answer_low_hotel_list()
 
-        # with open('location_answer', 'w+') as file:
-        #     json.dump(result, file, indent=4)
-
-        return result
-
-    def get_city():
-        result_location = get_location()
-        list_suggestions = result_location.get('suggestions')
-        group_city = list_suggestions[0]
-        # list_city_name = [name.get('name') for name in group_city.get('entities')] # returns a list of cities(
-        # city districts)
-        list_destinationId = [destinationId.get('destinationId') for destinationId in group_city.get('entities')]  #
-        # returns the ID of cities(city districts)
-        result = list_destinationId[0]
-
-        return result
-
-    def get_properties():
-        url = "https://hotels4.p.rapidapi.com/properties/list"
-
-        querystring = {
-            "destinationId": get_city(),
-            "pageNumber": "1",
-            "pageSize": "3",
-            "checkIn": "2022-08-08",
-            "checkOut": "2022-08-15",
-            "adults1": "1",
-            "sortOrder": "PRICE",
-            "locale": "en_US",
-            "currency": "USD"
-        }
-
-        headers = {
-            "X-RapidAPI-Key": "308a0ecd4dmsh778f749df86bb27p1e009ajsn881d6b34ec98",
-            "X-RapidAPI-Host": "hotels4.p.rapidapi.com"
-        }
-
-        response = requests.request("GET", url, headers=headers, params=querystring, timeout=10)
-        result = json.loads(response.text)
-
-        # with open('location_answer', 'w+') as file:
-        #     json.dump(result, file, indent=4)
-
-        return result
-
-    def get_hotels():
-
-        result_hotels = get_properties()
-
-        result = (result_hotels['data']['body']['searchResults']['results'])
-        # with open('hotel_answer', 'w+') as file:
-        #     json.dump(result, file, indent=4)
-
-        return result
-
-    def answer_hotel_list():
-
-        hotel_answer = get_hotels()
-
-        photo_count = want_photos(input("you want to see photos of hotels? ('yes'/'no'): ").lower())
-
-        answer = {}
-        for cur_hotel in hotel_answer:
-            hotel_id = cur_hotel.get("id")
-            address = cur_hotel.get("address")
-
-            current_price = "no price"
-            ratePlan = cur_hotel.get("ratePlan")
-            if not ratePlan:
-                answer.get("Price", "no price")
-            else:
-                price = ratePlan.get("price")
-                current_price = price.get("current")
-
-            guest_rating = "no rating"
-            guestReviews = cur_hotel.get("guestReviews")
-            if not guestReviews:
-                answer.get("Guest Rating", "no rating")
-            else:
-                guest_rating = guestReviews.get("rating")
-
-            answer = {
-                "Hotel name": cur_hotel.get("name"),
-                "Address": address.get("streetAddress", "no address"),
-                "Price": current_price,
-                "Guest rating": guest_rating
-            }
-            for answer_key, answer_value in answer.items():
-                print(answer_key + ":", answer_value)
-
-            if photo_count > 0:
-                get_hotel_photo(hotel_id, photo_count)
-
-    answer_hotel_list()
-
-
-low_price()
+# low_price()
